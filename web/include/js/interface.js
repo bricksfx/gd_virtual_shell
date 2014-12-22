dc40f27d8b5b40cc32aaff0086c3ae0317ec2465
*Chen <lin1994529@qq.com>
2014-12-22 21:02:38 +0800

    ADD the function of get history by ↑/↓

--- web/include/js/interface.js
+++ web/include/js/interface.js
@@ -14,6 +14,21 @@ $(function(){
         $("input:last").focus();        
         if (e.which == 13){
             $("input").attr("disabled", "disabled");
+            //记录命令历史
+            if (logined){
+                if (undefined != $.LS.get("command_stack")){
+                    now_command = command_array.length;
+                }
+                if (now_command != -1){      
+                    command_array.push($("input:last").val());
+                    now_command++;
+                } else {
+                    now_command++;                    
+                    command_array[now_command] = $("input:last").val();
+                }
+                
+                $.LS.set("command_stack", JSON.stringify(command_array));
+            }
             ShellCommand($("input:last").val());            
             $("input:last").focus();
             //逻辑运算
@@ -24,5 +39,10 @@ $(function(){
             AddCommandBox();
             $("input:last").focus();
         }
+        
+        if ((e.which == 38 || e.which == 40) && logined){
+            CommandHistory(e.which);
+        }
+        
     })
  })
